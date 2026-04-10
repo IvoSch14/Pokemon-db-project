1aa235115a1631a73d8d16924f8a62d7c308e67e
Keegan Jewell <kaj23f@fsu.edu>
2026-04-10 16:13:33 -0400

    Set up project structure, tested initial calls to api, flask app set up (still need to add routes that call service funcitons), need database schema before continuing

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Pokemon-db-project
+
+Tech Stack:
+- Python
+- Flask (backend)
+- SQLite 
+- TCGdex API (switch from scrydex as that was a paid api)
+- uv (for dependency management)
+
+uv usage:
+uv sync -- install all dependencies locally
+uv add {dependency} -- add to dependency list
+uv run {filename.py} -- run files with dependencies
---
+++ services/tcgdex_service.py
@@ -0,0 +1,38 @@
+from tcgdexsdk import TCGdex, Query
+
+tcgdex = TCGdex()
+
+
+# def test_card():
+#     try:
+#         card = tcgdex.card.getSync("swsh3-136")
+#         print("Connected successfully.")
+#         print(f"Name: {card.name}")
+#         print(f"Local ID: {card.localId}")
+#         print(f"Set: {card.set.name}")
+#         print(f"HP: {card.hp}")
+#         return card
+#     except Exception as e:
+#         print(f"TCGdex test failed: {e}")
+#         return None
+    
+def get_card_by_id(card_id):
+    return tcgdex.card.getSync(card_id)
+
+def search_cards_by_name(name: str):
+    return tcgdex.card.listSync(
+        Query().contains("name", name)
+    )
+
+def get_cards_by_set(set_id: str):
+    return tcgdex.card.listSync(
+        Query().equal("set.id", set_id)
+    )
+
+
+if __name__ == "__main__":
+    card = get_card_by_id("swsh3-136")
+    print(card.name if card else "No card found")
+
+    results = search_cards_by_name("charizard")
+    print(f"Found {len(results)} cards")
